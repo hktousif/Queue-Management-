dde0433f12d5c39fafdd34d290008f1d69ddde81
Tousif Kalalakond <46029444+hktousif@users.noreply.github.com>
2020-11-05 22:50:21 +0530

    Update settings.py

--- TimeSavityAPI/settings.py
+++ TimeSavityAPI/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8*aokwv*%x-eo2t)c24#y3)88$$x7%4_w043$@_&g^tibxd6wm'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
